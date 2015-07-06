b14b0251eadaa8a680e6cb9cdcde1266cc7a85c9
Erdong Feng <edfeng@thoughtworks.com>
2015-07-06 10:59:51 +0800

    use downstream collector in main collector.

--- src/main/java/io/fedcuit/github/java8/chapter5/Exercise2.java
+++ src/main/java/io/fedcuit/github/java8/chapter5/Exercise2.java
@@ -1,9 +1,12 @@
 package io.fedcuit.github.java8.chapter5;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.maxBy;
 
 public class Exercise2 {
@@ -14,5 +17,9 @@ public class Exercise2 {
 
     public static Optional<String> longestArtistNameByReduce(Stream<String> names) {
         return names.reduce((acc, x) -> x.length() > acc.length() ? x : acc);
+    }
+
+    public static Map<String, Long> countFrequency(Stream<String> names) {
+        return names.collect(groupingBy(word -> word, counting()));
     }
 }
--- src/test/java/io/fedcuit/github/java8/chapter5/Exercise2Test.java
+++ src/test/java/io/fedcuit/github/java8/chapter5/Exercise2Test.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import static io.fedcuit.github.java8.chapter5.Exercise2.longestArtistName;
-import static io.fedcuit.github.java8.chapter5.Exercise2.longestArtistNameByReduce;
+import static io.fedcuit.github.java8.chapter5.Exercise2.*;
 import static org.fest.assertions.Assertions.assertThat;
+import static org.fest.assertions.MapAssert.entry;
 
 public class Exercise2Test {
 
@@ -31,5 +31,15 @@ public class Exercise2Test {
         Optional<String> longestArtistName = longestArtistNameByReduce(names);
         assertThat(longestArtistName.isPresent()).isTrue();
         assertThat(longestArtistName.get()).isEqualTo("Stuart Sutcliffe");
+    }
+
+    @Test
+    public void shouldCountWordFrequency() throws Exception {
+        Stream<String> names = Stream.of("John", "Paul", "George", "John",
+                "Paul", "John");
+        assertThat(countFrequency(names))
+                .includes(entry("John", 3L))
+                .includes(entry("Paul", 2L))
+                .includes(entry("George", 1L));
     }
 }
