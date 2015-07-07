9eea42a6e9f29a4da661ae7ab65e025cb43f8dc1
Erdong Feng <edfeng@thoughtworks.com>
2015-07-07 09:53:43 +0800

    group data with reduce.(reduce() is ultimate way to make a result from a stream, collect() is the shortcuts of reduce).

---
+++ src/main/java/io/fedcuit/github/java8/chapter5/MyGroupBy.java
@@ -0,0 +1,4 @@
+package io.fedcuit.github.java8.chapter5;
+
+public class MyGroupBy {
+}
---
+++ src/test/java/io/fedcuit/github/java8/chapter5/MyGroupByTest.java
@@ -0,0 +1,48 @@
+package io.fedcuit.github.java8.chapter5;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class MyGroupByTest {
+
+    private Stream<String> companies;
+
+    @Before
+    public void setUp() throws Exception {
+        companies = Stream.of("Lvl1 Google", "Lvl2 MS", "Lvl1 Apple", "Lvl3 Yahoo");
+    }
+
+    @Test
+    public void shouldGroupDataByNativeAPI() throws Exception {
+        Map<String, List<String>> companiesByLevel = companies.collect(Collectors.groupingBy(s -> s.substring(0, 4)));
+        assertThat(companiesByLevel.get("Lvl1")).contains("Lvl1 Google", "Lvl1 Apple");
+        assertThat(companiesByLevel.get("Lvl2")).contains("Lvl2 MS");
+        assertThat(companiesByLevel.get("Lvl3")).contains("Lvl3 Yahoo");
+    }
+
+    @Test
+    public void shouldGroupDataByReduce() throws Exception {
+        Map<String, List<String>> companiesByLevel = companies.reduce(new HashMap<>(), (acc, x) -> {
+            String classifier = x.substring(0, 4);
+            List<String> group = acc.computeIfAbsent(classifier, __ -> new ArrayList<>());
+            group.add(x);
+            return acc;
+        }, (left, right) -> {
+            left.putAll(right);
+            return left;
+        });
+
+        assertThat(companiesByLevel.get("Lvl1")).contains("Lvl1 Google", "Lvl1 Apple");
+        assertThat(companiesByLevel.get("Lvl2")).contains("Lvl2 MS");
+        assertThat(companiesByLevel.get("Lvl3")).contains("Lvl3 Yahoo");
+    }
+}
