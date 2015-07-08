bcdb4762a719c5bdf0897893a9c9a378e60d85c0
Erdong Feng <edfeng@thoughtworks.com>
2015-07-08 11:08:51 +0800

    join lines.

--- src/main/java/io/fedcuit/github/java8/chapter6/Ch6Exercise2.java
+++ src/main/java/io/fedcuit/github/java8/chapter6/Ch6Exercise2.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 public class Ch6Exercise2 {
     public static int multiplyThrough(List<Integer> linkedListOfNumbers) {
-        Integer product = linkedListOfNumbers.stream().parallel()
+        return 5 * linkedListOfNumbers.stream().parallel()
                 .reduce(1, (acc, x) -> x * acc);
-        return product * 5;
     }
 
     public static long slowSumOfSquares(List<Long> linkedListOfNumbers) {
